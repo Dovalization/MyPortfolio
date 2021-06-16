914f76aeb8d652223eb4df193d988cc739a2b9f7
Guilherme Doval <doval.guilherme94@gmail.com>
2021-06-16 14:03:57 -0300

    disables eslint for builds

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   images: {
     domains: ['images.prismic.io', 'prismic-io.s3.amazonaws.com', 'guilhermedovalportfolio.cdn.prismic.io']
-  }
+  },
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
